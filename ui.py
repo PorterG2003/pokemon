ea97865579c033e2e61f76ad8963fca8fa4de82c
PorterG2003 <porterg2003@gmail.com>
2023-12-10 16:59:12 -0700

    Working Version

--- ui.py
+++ ui.py
@@ -27,37 +27,38 @@ def battle():
     name2 = name2.capitalize()
     db.battle(name1, name2)
 
-quit = False
-print("\nWELCOME TO POKÉMON APP\n\n")
-menu()
+if __name__ == '__main__':
+    quit = False
+    print("\nWELCOME TO POKÉMON APP\n\n")
+    menu()
 
-while not quit:
-    choice = input("\n\nchoice: ")
-    if choice == "team":
-        team_stats()
-    elif choice == "type":
-        average_stats_by_type()
-    elif choice == "battle":
-        battle()
-    elif choice == "add":
-        pokemon = input("Which pokemon do you want to add? ")
-        pokemon = pokemon.capitalize()
-        db.add_to_team(pokemon)
-    elif choice == "remove":
-        pokemon = input("Which pokemon would you like to remove? ")
-        pokemon = pokemon.capitalize()
-        db.remove_from_team(pokemon)
-    elif choice == "fill":
-        db.fill_team()
-    elif choice == "most specialized":
-        db.most_specialized_pokemon_by_stat()
-    elif choice == "clear":
-        db.clear_team()
-    elif choice == "top pokemon":
-        db.top_pokemon_by_stat()
-    elif choice == "menu":
-        menu()
-    elif choice == "quit":
-        quit = True
-    else:
-        print("### Enter a valid choice ###\n")
+    while not quit:
+        choice = input("\n\nchoice: ")
+        if choice == "team":
+            team_stats()
+        elif choice == "type":
+            average_stats_by_type()
+        elif choice == "battle":
+            battle()
+        elif choice == "add":
+            pokemon = input("Which pokemon do you want to add? ")
+            pokemon = pokemon.capitalize()
+            db.add_to_team(pokemon)
+        elif choice == "remove":
+            pokemon = input("Which pokemon would you like to remove? ")
+            pokemon = pokemon.capitalize()
+            db.remove_from_team(pokemon)
+        elif choice == "fill":
+            db.fill_team()
+        elif choice == "most specialized":
+            db.most_specialized_pokemon_by_stat()
+        elif choice == "clear":
+            db.clear_team()
+        elif choice == "top pokemon":
+            db.top_pokemon_by_stat()
+        elif choice == "menu":
+            menu()
+        elif choice == "quit":
+            quit = True
+        else:
+            print("### Enter a valid choice ###\n")
